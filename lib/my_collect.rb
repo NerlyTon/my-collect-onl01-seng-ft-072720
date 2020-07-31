7e106ad48f156766356010b0eeb6510af30663cd
Nerly Ton <nerlyton@gmail.com>
2020-07-31 18:04:06 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,10 @@
 def my_collect(array)
+  i = 0
   new_collection = []
+  while i < array.length
   collection = array.collect do |lang|
     lang.upcase
-  new_collection << yield(array)
+  new_collection << yield(array[i])
   end
   new_collection
 end
