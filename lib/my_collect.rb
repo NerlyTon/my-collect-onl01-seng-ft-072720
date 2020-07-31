7ff3b98e0e18de19f951cd424591775d52401be8
Nerly Ton <nerlyton@gmail.com>
2020-07-31 18:09:38 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
-
+def my_collect(array)
+  i = 0
+  new_collection = []
+  while i < array.length
+  # collection = array.collect do |lang|
+  #   lang.upcase
+  new_collection.push yield(array[i])
+  i += 1
+  end
+  new_collection
+end
 
